2d53ef21be5e1d8c7594785b0a8afb151439ef64
Alex Alsheuski <alex@alsheuski.com>
2025-10-03 08:12:43 +0300

    add facilities api test

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -5,9 +5,16 @@ async def test_get_facilities(ac: AsyncClient):
     response = await ac.get("/facilities")
 
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
 
 async def test_add_facility(ac: AsyncClient):
-    response = await ac.post("/facilities", json={"title": "WI-FI"})
+    facility_title = "WI-FI"
+    response = await ac.post("/facilities", json={"title": facility_title})
 
     assert response.status_code == 200
+    res = response.json()
+
+    assert isinstance(res, dict)
+    assert res["title"] == facility_title
+    assert "data" in res
